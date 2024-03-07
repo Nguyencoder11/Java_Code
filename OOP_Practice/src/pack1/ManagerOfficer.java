56a646bcd6063469279a70457cc75bb543d896ef
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-03-07 18:09:45 +0700

    init package1

---
+++ OOP_Practice/src/pack1/ManagerOfficer.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pack1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ManagerOfficer {
+
+    private final List<Officer> officers;
+
+    public ManagerOfficer() {
+        this.officers = new ArrayList<>();
+    }
+
+    public void addOfficer(Officer officer) {
+        this.officers.add(officer);
+    }
+
+    public List<Officer> searchOfficerByName(String name) {
+        return this.officers.stream().filter(o -> o.getName().contains(name)).collect(Collectors.toList());
+    }
+
+    public void showListInforOfficer() {
+        this.officers.forEach(o -> System.out.println(o.toString()));
+    }
+}
